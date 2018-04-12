bd73b6db5b2320cde732f8c40ab5ccbfedd53b3e
Matthew Klein <mklein994@gmail.com>
2018-04-12 09:53:17 -0500

    Add firefox launcher to karma

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
