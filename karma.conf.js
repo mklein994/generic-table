21dfcf9b96a2f845ad6b71257a694f4452a8e386
Matthew Klein <mklein994@gmail.com>
2018-04-13 11:33:26 -0500

    Remove dotenv setup from karma
    It's easier to just export browser paths in ~/.profile.

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-require('dotenv').config();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
